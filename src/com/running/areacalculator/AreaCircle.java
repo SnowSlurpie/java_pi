970def9496cf038f305b02dc820cce8c054bcd6d
SnowSlurpie <runniale000@gmail.com>
2022-11-16 06:59:29 -0800

    pushing pop quiz

---
+++ src/com/running/areacalculator/AreaCircle.java
@@ -0,0 +1,17 @@
+package com.running.areacalculator;
+
+public class AreaOfACircle {
+	
+	private static double number = 3.14, area;
+	private static int radius;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		radius=5;
+		area = number * radius * radius;
+		
+		System.out.println(area);
+	}
+}
+
+	
